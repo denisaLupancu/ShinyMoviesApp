94928cc16d532a97cc8d8233501b005f3916c935
Denisa Lupancu <310440501rsl201117@feaa.uaic.ro>
2023-06-26 20:35:21 +0300

    fixare port

--- run.R
+++ run.R
@@ -1,10 +1,12 @@
 library(shiny)
 
-port<-7347
-Sys.setenv(PORT = port)
+port <- Sys.getenv('PORT', "8080")  # Setează portul ca fiind 8080 dacă variabila de mediu PORT nu este disponibilă
+
+# Definește o adresă IP pentru host în funcție de variabila de mediu din Heroku
+host <- if (Sys.getenv('PORT') == '') '0.0.0.0' else '127.0.0.1'
 
 shiny::runApp(
   appDir = getwd(),
-  host = '127.0.0.1',
+  host = host,
   port = as.numeric(port)
 )
