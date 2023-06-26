c2fedd514433c49f29d4d3b6da6f30650b63633c
Denisa Lupancu <310440501rsl201117@feaa.uaic.ro>
2023-06-26 21:54:08 +0300

    modify init file

---
+++ README.md
@@ -0,0 +1,4 @@
+# ShinyMoviesApp
+Aplicația este realizată în cadrul lucrării științifice de finalizare a studiilor de licență - promoția iulie 2023.
+
+Scopul acestei aplicații constă în îmbunătățirea procesului de luare a deciziilor economice în domeniul cinematografic.
---
+++ run.R
@@ -0,0 +1,12 @@
+library(shiny)
+
+port <- Sys.getenv('PORT', "8080")  # Setează portul ca fiind 8080 dacă variabila de mediu PORT nu este disponibilă
+
+# Definește o adresă IP pentru host în funcție de variabila de mediu din Heroku
+host <- if (Sys.getenv('PORT') == '') '0.0.0.0' else '127.0.0.1'
+
+shiny::runApp(
+  appDir = getwd(),
+  host = host,
+  port = as.numeric(port)
+)
