b43bfece872d9614081b511cab228728f838fdfe
Denisa Lupancu <310440501rsl201117@feaa.uaic.ro>
2023-06-27 19:45:36 +0300

    quick fix

--- init.R
+++ init.R
@@ -17,7 +17,7 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("shiny", "tidyverse", "shinyalert", "shinyauthr", "textshaping", "ragg")
+my_packages = c("tidyverse","shiny","shinyalert", "shinyauthr", "textshaping", "ragg")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
